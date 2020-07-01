d144733790364090823de47d6dffcbb37758336b
yhh1056 <yhh1056@naver.com>
2020-07-02 00:04:27 +0900

    가운데 글자 가리기

---
+++ src/programmers/p12903/Test.java
@@ -0,0 +1,22 @@
+package programmers.p12903;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("yhh1056"));
+    }
+
+
+}
+class Solution {
+    public String solution(String s) {
+        int len = s.length();
+        int middleNumber = len / 2;
+
+        if (s.length() / 2 != 0) {
+            return s.substring(middleNumber, middleNumber + 1);
+        }
+        return s.substring(middleNumber - 1, middleNumber + 1);
+
+    }
+}
