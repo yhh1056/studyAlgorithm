9080f8f676d5696617eb35043022cc9db6a171b6
yhh1056 <yhh1056@naver.com>
2020-07-02 00:03:55 +0900

    핸드폰 번호 가리기

--- src/programmers/p12948/Test.java
+++ src/programmers/p12948/Test.java
@@ -1,23 +1,11 @@
 package programmers.p12948;
-
-/**
- * 프로그래머스 핸드폰 번호 가리기 문제;
- *
- * author {yhh1056}
- * Create by {2020/06/29}
- */
 public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("01012345678"));
+    }
 
-        public static void main(String[] args) {
-            Solution solution = new Solution();
-            solution.solution("01012341234");
-
-            Solution2 solution2 = new Solution2();
-            solution2.solution("01012341234");
-
-        }
 }
-
 class Solution {
     public String solution(String phone_number) {
         StringBuilder encryptionPhoneNumber = new StringBuilder();
@@ -30,15 +18,5 @@ class Solution {
         }
         encryptionPhoneNumber = encryptionPhoneNumber.append(showPhoneNumber);
         return String.valueOf(encryptionPhoneNumber);
-    }
-}
-
-class Solution2 {
-    public String solution(String phone_number) {
-        char[] answer = phone_number.toCharArray();
-        for (int i = 0; i < answer.length - 4; i++) {
-            answer[i] = '*';
-        }
-        return String.valueOf(answer);
     }
 }
