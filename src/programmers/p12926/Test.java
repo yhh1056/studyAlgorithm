a216990a958f794b5e5483f51e0a9f4641622bd6
yhh1056 <yhh1056@naver.com>
2020-07-22 18:53:29 +0900

    시저 암호

---
+++ src/programmers/p12926/Test.java
@@ -0,0 +1,45 @@
+package programmers.p12926;
+
+
+/**
+ * author {yhh1056}
+ * Create by {2020/07/22}
+ */
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution("x Y", 3);
+        System.out.println(10 % 26);
+    }
+}
+
+class Solution {
+    public String solution(String s, int n) {
+        String answer;
+        char space = 32;
+
+        char[] chars = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == space) {
+                chars[i] = space;
+            } else {
+                chars[i] = s.charAt(i);
+            }
+        }
+
+        for (int i = 0; i < chars.length; i ++) {
+            if (chars[i] == space ) {
+                chars[i] = space;
+            } else if (chars[i] <= 90 && chars[i] + n > 90) {
+                chars[i] += n - 26;
+            } else if (chars[i] <= 122 && chars[i] + n > 122) {
+                chars[i] += n - 26;
+            } else {
+                chars[i] += n;
+            }
+        }
+        answer = String.valueOf(chars);
+        System.out.println(answer);
+        return answer;
+    }
+}
