cb8f9574822c816047f10f160d7d69189ca8f51b
yhh1056 <yhh1056@naver.com>
2020-08-12 14:03:08 +0900

    위장

---
+++ src/programmers/p42578/Test.java
@@ -0,0 +1,48 @@
+package programmers.p42578;
+
+import java.util.HashMap;
+
+/**
+ * author {yhh1056}
+ * Create by {2020/08/12}
+ */
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution(new String[][]{{"yellow_hat", "headgear"},
+                {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}});
+    }
+}
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        int answer = 1;
+        for (String[] clothe : clothes) {
+            if (hashMap.containsKey(clothe[1])) {
+                hashMap.put(clothe[1], hashMap.get(clothe[1]) + 1);
+            } else {
+                hashMap.put(clothe[1], 1);
+            }
+        }
+        for (String key : hashMap.keySet()) {
+            answer *= (hashMap.get(key) + 1);
+        }
+        System.out.println(hashMap);
+        System.out.println(answer - 1);
+        return answer - 1;
+    }
+}
+
+//for (int i = 0; i < clothes.length; i++) {
+//        if (hashMap.containsKey(clothes[i][1])) {
+//        list = hashMap.get(clothes[i][1]);
+//        } else {
+//        list = new ArrayList<>();
+//        }
+//        list.add(clothes[i][0]);
+//        hashMap.put(clothes[i][1], list);
+//        }
+//        if (hashMap.size() == 1) {
+//        return hashMap.size();
+//        }
