2dc3dce361f0f109b2a5ebd4a70c9c3c54f49b81
yhh1056 <yhh1056@naver.com>
2020-08-24 14:14:55 +0900

    다리를 지나는 트럭

--- src/programmers/p42583/Test.java
+++ src/programmers/p42583/Test.java
@@ -6,37 +6,38 @@ import java.util.Queue;
 /**
  * author {yhh1056}
  * Create by {2020/08/11}
+ * <p>
+ * 다리를 지나나는 터널
  */
 public class Test {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.solution(2, 10, new int[]{7,4,5,6});
+        solution.solution(2, 10, new int[]{7, 4, 5, 6});
     }
 }
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> queue = new LinkedList<>();
-        int count = 0;
+        Queue<Integer> bridge = new LinkedList<>();
+        int countTime = 0;
         int max = 0;
+        int virtualTruck = 0;
 
         for (int i = 0; i < truck_weights.length; i++) {
             max += truck_weights[i];
             if (max <= weight) {
-                queue.offer(truck_weights[i]);
-            } else  {
-                queue.offer(0);
+                bridge.offer(truck_weights[i]);
+            } else {
+                bridge.offer(virtualTruck);
                 max -= truck_weights[i];        //트럭 무게 다시 빼준다.
                 i -= 1;
             }
-            count++;
-            if (queue.size() == bridge_length) {
-                max -= queue.peek();
-                queue.poll();
+            countTime++;
+            if (bridge.size() == bridge_length) {
+                max -= bridge.poll();
             }
-
         }
-        return count + bridge_length;               //마지막 트럭이 지나가는 거리;
+        return countTime + bridge_length;               //마지막 트럭이 지나가는 거리;
     }
 }
 
