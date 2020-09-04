a5bc8c2ae3eb18ecca7a5f834b47c2c7ffba24b7
yhh1056 <yhh1056@naver.com>
2020-09-04 10:51:34 +0900

    삽입 정렬

---
+++ src/sortPractice/InsertionSort.java
@@ -0,0 +1,28 @@
+package sortPractice;
+
+/**
+ * author {yhh1056}
+ * Create by {2020/09/04}
+ */
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] array = {9, 3, 1, 9, 5, 6, -1};
+        int length = array.length;
+
+        for (int i = 1; i < length; i++) {
+            int j = i;
+            int key = array[i];
+
+            while (j > 0 && array[j - 1] > key) {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = key;
+
+            for (int value : array) {
+                System.out.printf("%3d", value);
+            }
+            System.out.println();
+        }
+    }
+}
