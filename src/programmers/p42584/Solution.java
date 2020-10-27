704315a7ec713f9254c879a08eaa5a33496ca516
yhh1056 <yhh1056@naver.com>
2020-10-27 22:43:31 +0900

    주식 가격

--- src/programmers/p42584/Solution.java
+++ src/programmers/p42584/Solution.java
@@ -1,3 +1,19 @@
 package programmers.p42584;
 
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length - 1; i++) {
+            int price = prices[i];
+            int index = i;
+            int count = 0;
 
+            while (index < prices.length - 1 && price <= prices[index]) {
+                index++;
+                count++;
+            }
+            answer[i] = count;
+        }
+        return answer;
+    }
+}
