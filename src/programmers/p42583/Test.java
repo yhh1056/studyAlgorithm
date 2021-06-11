77cc491d1e31a9b39480818b56fd20710a0df295
yhh1056 <yhh1056@naver.com>
2021-06-11 15:02:09 +0900

    다리를 지나나는 터널 재풀이

--- src/programmers/p42583/Test.java
+++ src/programmers/p42583/Test.java
@@ -10,34 +10,54 @@ import java.util.Queue;
  * 다리를 지나나는 터널
  */
 public class Test {
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.solution(2, 10, new int[]{7, 4, 5, 6});
+        solution.solution(100, 100, new int[]{10});
     }
 }
 
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> bridge = new LinkedList<>();
-        int countTime = 0;
-        int max = 0;
-        int virtualTruck = 0;
 
-        for (int i = 0; i < truck_weights.length; i++) {
-            max += truck_weights[i];
-            if (max <= weight) {
-                bridge.offer(truck_weights[i]);
-            } else {
-                bridge.offer(virtualTruck);
-                max -= truck_weights[i];        //트럭 무게 다시 빼준다.
-                i -= 1;
-            }
-            countTime++;
-            if (bridge.size() == bridge_length) {
-                max -= bridge.poll();
-            }
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Queue<Integer> bridge = new LinkedList<>();
+        Queue<Integer> truck = new LinkedList<>();
+
+        for (int truck_weight : truck_weights) {
+            truck.offer(truck_weight);
         }
-        return countTime + bridge_length;               //마지막 트럭이 지나가는 거리;
+
+        for (int i = 0; i < bridge_length; i++) {
+            bridge.offer(0);
+        }
+
+        while (!truck.isEmpty()) {
+            answer++;
+            bridge.poll();
+
+            int total = getTotal(bridge) + truck.peek();
+
+            if (weight >= total) {
+                bridge.offer(truck.poll());
+            } else {
+                bridge.offer(0);
+            }
+
+        }
+
+        return answer + bridge_length;
+
     }
+
+    private int getTotal(Queue<Integer> bridge) {
+        int total = 0;
+        for (int b : bridge) {
+            total += b;
+        }
+        return total;
+    }
+
 }
 
