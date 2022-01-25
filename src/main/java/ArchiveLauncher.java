167aa69ce51297b60d1aca4edaa7708b770222b6
yhh1056 <yhh1056@naver.com>
2022-01-26 00:38:15 +0900

    경비원 코드 리팩토링 및 아카이브 저장 위치 수정
    경비원 코드 리팩토링 및 아카이브 저장 위치 수정

--- src/main/java/ArchiveLauncher.java
+++ src/main/java/ArchiveLauncher.java
@@ -4,7 +4,7 @@ import archive.algorithm.file.MarkdownGenerator;
 
 public class ArchiveLauncher {
     public static void main(String[] args) {
-        GithubConfig config = new GithubConfig("yhh1056", "repo", "main");
+        GithubConfig config = new GithubConfig("yhh1056", "studyAlgorithm", "main");
         ArchiveLauncher launcher = new ArchiveLauncher();
         launcher.run(new DefaultBaekjoonMarkdownGenerator(config));
     }
--- src/main/java/baekjoon/b2564/Main.java
+++ src/main/java/baekjoon/b2564/Main.java
@@ -17,40 +17,64 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
 
         int[] load = new int[M * 2 + N * 2];
-        int size = Integer.parseInt(br.readLine());
+        int storeCount = Integer.parseInt(br.readLine());
 
-        int count = 1;
-        for (int i = 0; i < size; i++) {
+        int point = 1;
+
+        /**
+         * 동서남북에 위치한 상점들을 load에 담아둔다.
+         * point 변수는 상점들을 차례대로 탐색하기 위해 사용한다.
+         */
+        for (int i = 0; i < storeCount; i++) {
             st = new StringTokenizer(br.readLine());
             int way = Integer.parseInt(st.nextToken());
             int distance = Integer.parseInt(st.nextToken());
 
             // 북
             if (way == 1) {
-                load[distance] = count++;
+                load[distance] = point++;
             }
 
             // 남
             if (way == 2) {
-                load[M + N + M - distance] = count++;
+                load[M + N + M - distance] = point++;
             }
 
             // 서
             if (way == 3) {
-                load[M + N + M + N - distance] = count++;
+                load[M + N + M + N - distance] = point++;
             }
 
             // 동
             if (way == 4) {
-                load[M + distance] = count++;
+                load[M + distance] = point++;
             }
         }
 
-        int now = 0;
         st = new StringTokenizer(br.readLine());
         int way = Integer.parseInt(st.nextToken());
         int distance = Integer.parseInt(st.nextToken());
 
+        /**
+         * 마지막줄의 입력은 동근이의 위치정보가 나온다.
+         * 동근이의 위치를 파악하여 현재 지점을 now 변수에 저장한다.
+         */
+        int now = setNow(M, N, way, distance);
+
+        int answer = 0;
+
+        /**
+         * 이전에 저장해둔 point 지점대로 탐색한다.
+         * 왼쪽으로 출발해보고, 오른쪽으로 출발하여 각 상점의 최단 거리를 구한다.
+         */
+        for (int i = 1; i < point; i++) {
+            answer += Math.min(left(i, load, now), right(i, load, now));
+        }
+        System.out.println(answer);
+    }
+
+    private static int setNow(int M, int N, int way, int distance) {
+        int now = 0;
         if (way == 1) {
             now = distance;
         }
@@ -66,42 +90,29 @@ public class Main {
         if (way == 4) {
             now = M + distance;
         }
-
-        int answer = 0;
-        for (int i = 1; i < count; i++) {
-            answer += Math.min(left(i, load, now), right(i, load, now));
-        }
-        System.out.println(answer);
+        return now;
     }
 
     private static int left(int target, int[] load, int now) {
         int count = 0;
-        while (true) {
-            if (load[now] == target) {
-                break;
-            } else {
-                now -= 1;
-                if (now == -1) {
-                    now = load.length - 1;
-                }
-                count++;
+        while (load[now] != target) {
+            now -= 1;
+            if (now == -1) {
+                now = load.length - 1;
             }
+            count++;
         }
         return count;
     }
 
     private static int right(int target, int[] load, int now) {
         int count = 0;
-        while (true) {
-            if (load[now] == target) {
-                break;
-            } else {
-                now += 1;
-                if (now == load.length) {
-                    now = 0;
-                }
-                count++;
+        while (load[now] != target) {
+            now += 1;
+            if (now == load.length) {
+                now = 0;
             }
+            count++;
         }
         return count;
     }
